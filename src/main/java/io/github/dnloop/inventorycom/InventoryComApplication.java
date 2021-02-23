bc0565015178e43681641c5345a69591b0912b83
dnloop <dnloopdev@gmail.com>
2021-02-23 17:45:43 -0300

    Enabled Async configuration.

--- src/main/java/io/github/dnloop/inventorycom/InventoryComApplication.java
+++ src/main/java/io/github/dnloop/inventorycom/InventoryComApplication.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @EnableJpaRepositories("io.github.dnloop.inventorycom.repository")
 @EntityScan("io.github.dnloop.inventorycom.model")
 public class InventoryComApplication {
