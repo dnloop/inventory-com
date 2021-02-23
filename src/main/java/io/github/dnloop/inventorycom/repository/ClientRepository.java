6681476d5e39d1110e3a9e44fd1674af232be5c4
dnloop <dnloopdev@gmail.com>
2021-02-23 09:47:58 -0300

    Wrong package, missing argument.

--- src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
+++ src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
@@ -2,14 +2,13 @@ package io.github.dnloop.inventorycom.repository;
 
 import io.github.dnloop.inventorycom.model.Client;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.awt.print.Pageable;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     Page<Client> findClientsByOrderBySurnameAsc(Pageable pageable);
 
     @Query("SELECT client FROM Client  client WHERE  client.deleted = 1")
-    Page<Client> findAllDeleted();
+    Page<Client> findAllDeleted(Pageable pageable);
 }
