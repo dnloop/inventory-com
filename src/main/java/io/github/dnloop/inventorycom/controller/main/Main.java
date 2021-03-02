79d75212dc8e3029ec178d9624765796d3e1f833
dnloop <dnloopdev@gmail.com>
2021-03-02 19:13:29 -0300

    Added async initialization.

--- src/main/java/io/github/dnloop/inventorycom/controller/main/Main.java
+++ src/main/java/io/github/dnloop/inventorycom/controller/main/Main.java
@@ -1,8 +1,5 @@
 package io.github.dnloop.inventorycom.controller.main;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import io.github.dnloop.inventorycom.utils.RouteCrud;
 import io.github.dnloop.inventorycom.utils.RouteInvoice;
 import io.github.dnloop.inventorycom.utils.RouteMain;
@@ -15,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
@@ -76,77 +74,78 @@ public class Main {
 
     @FXML
     void about(ActionEvent event) {
-	SceneManager.loadView(RouteMain.MAIN.about());
+        SceneManager.loadView(RouteMain.MAIN.about());
     }
 
     @FXML
     void exit(ActionEvent event) {
-	Platform.exit();
+        Platform.exit();
     }
 
     @FXML
     void newCategory(ActionEvent event) {
-	SceneManager.loadModal(RouteCrud.PRODUCT.extra(), "Categorías", true);
+        SceneManager.loadModal(RouteCrud.PRODUCT.extra(), "Categorías", true);
     }
 
     @FXML
     void newClient(ActionEvent event) {
-	SceneManager.loadModal(RouteCrud.CLIENT.create(), "Nuevo Cliente", true);
+        SceneManager.loadModal(RouteCrud.CLIENT.create(), "Nuevo Cliente", true);
     }
 
     @FXML
     void newProduct(ActionEvent event) {
-	SceneManager.loadModal(RouteCrud.PRODUCT.create(), "Nuevo Producto", true);
+        SceneManager.loadModal(RouteCrud.PRODUCT.create(), "Nuevo Producto", true);
     }
 
     @FXML
     void newSupplier(ActionEvent event) {
-	SceneManager.loadModal(RouteCrud.SUPPLIER.create(), "Nuevo Proveedor", true);
+        SceneManager.loadModal(RouteCrud.SUPPLIER.create(), "Nuevo Proveedor", true);
     }
 
     @FXML
     void showBuys(ActionEvent event) {
-	SceneManager.loadView(RouteInvoice.BUYS.display());
+        SceneManager.loadView(RouteInvoice.BUYS.display());
     }
 
     @FXML
     void showCategory(ActionEvent event) {
-	SceneManager.loadModal(RouteCrud.PRODUCT.extra(), "Categorías", true);
+        SceneManager.loadModal(RouteCrud.PRODUCT.extra(), "Categorías", true);
     }
 
     @FXML
     void showClients(ActionEvent event) {
-	SceneManager.loadView(RouteCrud.CLIENT.display());
+        SceneManager.loadView(RouteCrud.CLIENT.display());
     }
 
     @FXML
     void showInventory(ActionEvent event) {
-	SceneManager.loadView(RouteCrud.PRODUCT.display());
+        SceneManager.loadView(RouteCrud.PRODUCT.display());
     }
 
     @FXML
     void showMain(ActionEvent event) {
-	SceneManager.loadView(RouteMain.MAIN.dataEntry());
+        SceneManager.loadView(RouteMain.MAIN.dataEntry());
     }
 
     @FXML
     void showSales(ActionEvent event) {
-	SceneManager.loadView(RouteInvoice.SALES.display());
+        SceneManager.loadView(RouteInvoice.SALES.display());
     }
 
     @FXML
     void showSuppliers(ActionEvent event) {
-	SceneManager.loadView(RouteCrud.SUPPLIER.display());
+        SceneManager.loadView(RouteCrud.SUPPLIER.display());
     }
 
     @FXML
     void initialize() {
-        Platform.runLater(()-> {
-            SceneManager.loadView(RouteMain.MAIN.dataEntry());
-        });
+        Platform.runLater(() -> SceneManager.loadView(
+                RouteMain.MAIN.dataEntry())
+        );
     }
 
+    @Async
     public void setView(Node node) {
-	mainPane.setCenter(node);
+        mainPane.setCenter(node);
     }
 }
