e1358bf7d3f11cd07f1be7438629ba9b9825a5b8
dnloop <dnloopdev@gmail.com>
2021-03-18 23:39:32 -0300

    Added query for deleted entity.

--- src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
+++ src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Integer> {
     Page<Client> findClientsByOrderBySurnameAsc(Pageable pageable);
 
-    @Query("SELECT client FROM Client  client WHERE  client.deleted = 1")
+    @Query("SELECT client FROM Client client WHERE  client.deleted = 1")
     Page<Client> findAllDeleted(Pageable pageable);
+
+    @Query("SELECT client FROM Client client WHERE client.id = :id AND client.deleted = 1")
+    Optional<Client> findDeleted(int id);
 }
