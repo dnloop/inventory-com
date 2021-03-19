1d7fdac7be69e8e3861756352b03804cbbadd2a6
dnloop <dnloopdev@gmail.com>
2021-03-18 22:16:03 -0300

    Removed quotes preventing column find.

--- src/test/resources/db/data/localities.sql
+++ src/test/resources/db/data/localities.sql
@@ -13,5 +13,5 @@ insert into inventario_comercial.municipality
 values(1, 'MUNICIPALITY');
 
 insert into inventario_comercial.locality
-(id, category, centroid_lat, centroid_lon, departament_id, "function", municipality_id, name, province_id)
+(id, category, centroid_lat, centroid_lon, departament_id, function, municipality_id, name, province_id)
 values(1, 'CATEGORY', 1, 1, 1, 'FUNCTION', 1, 'LOCALITY', 1);
