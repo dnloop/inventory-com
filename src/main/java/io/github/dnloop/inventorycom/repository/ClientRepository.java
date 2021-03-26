cb760e663ae8f01d8eac1da4338066502ca135af
dnloop <dnloopdev@gmail.com>
2021-03-26 10:31:29 -0300

    Removed unused method.

--- src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
+++ src/main/java/io/github/dnloop/inventorycom/repository/ClientRepository.java
@@ -1,7 +1,6 @@
 package io.github.dnloop.inventorycom.repository;
 
 import io.github.dnloop.inventorycom.model.Client;
-import org.hibernate.annotations.Where;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,14 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-    Page<Client> findClientsByOrderBySurnameAsc(Pageable pageable);
 
-    @Query("SELECT client FROM Client client WHERE client.deleted = 0")
+    @Query("SELECT client FROM Client client" +
+           " WHERE client.deleted = 0")
     Page<Client> findAll(Pageable pageable);
 
-    @Query("SELECT client FROM Client client WHERE client.deleted = 1")
+    @Query("SELECT client FROM Client client" +
+           " WHERE client.deleted = 1" +
+           " ORDER BY client.deletedAt")
     Page<Client> findAllDeleted(Pageable pageable);
 
-    @Query("SELECT client FROM Client client WHERE client.id = :id AND client.deleted = 1")
+    @Query("SELECT client FROM Client client" +
+           " WHERE client.id = :id AND client.deleted = 1")
     Optional<Client> findDeleted(int id);
 }
