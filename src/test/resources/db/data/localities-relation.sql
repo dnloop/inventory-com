f420ab00104facd6ea470b159925087a4be26c0a
dnloop <dnloopdev@gmail.com>
2021-03-29 10:51:24 -0300

    Added Test data.

--- src/test/resources/db/data/localities-relation.sql
+++ src/test/resources/db/data/localities-relation.sql
@@ -1,6 +1,8 @@
 insert into PUBLIC.province
 (id, name)
-values(1, 'PROVINCE');
+values(1, 'PROVINCE-1'),
+      (2, 'PROVINCE-1'),
+      (3, 'PROVINCE-1');
 
 
 insert into PUBLIC.departments
@@ -18,6 +20,6 @@ insert into PUBLIC.locality
 (id, category, centroid_lat, centroid_lon, departament_id, function, municipality_id, name, province_id)
 values(1, 'CATEGORY', 1, 1, 1, 'FUNCTION', 1, 'LOCALITY-1', 1),
       (2, 'CATEGORY', 1, 1, 1, 'FUNCTION', 1, 'LOCALITY-2', 1),
-      (3, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-3', 1),
-      (4, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-4', 1),
-      (5, 'CATEGORY', 1, 1, 1, 'FUNCTION', 3, 'LOCALITY-5', 1);
+      (3, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-3', 2),
+      (4, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-4', 2),
+      (5, 'CATEGORY', 1, 1, 1, 'FUNCTION', 3, 'LOCALITY-5', 3);
