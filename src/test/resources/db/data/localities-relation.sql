9bc058f331621815f2e99f1b7b80f5f75a7bfe23
dnloop <dnloopdev@gmail.com>
2021-03-31 10:02:20 -0300

    Increased test rows.

--- src/test/resources/db/data/localities-relation.sql
+++ src/test/resources/db/data/localities-relation.sql
@@ -7,7 +7,8 @@ values(1, 'PROVINCE-1'),
 
 insert into PUBLIC.departments
 (id, name)
-values(1, 'DEPARTMENT');
+values(1, 'DEPARTMENT-1'),
+      (2, 'DEPARTMENT-2');
 
 
 insert into PUBLIC.municipality
@@ -18,8 +19,8 @@ values(1, 'MUNICIPALITY-1'),
 
 insert into PUBLIC.locality
 (id, category, centroid_lat, centroid_lon, departament_id, function, municipality_id, name, province_id)
-values(1, 'CATEGORY', 1, 1, 1, 'FUNCTION', 1, 'LOCALITY-1', 1),
-      (2, 'CATEGORY', 1, 1, 1, 'FUNCTION', 1, 'LOCALITY-2', 1),
+values(1, 'CATEGORY', 1, 1, 2, 'FUNCTION', 1, 'LOCALITY-1', 1),
+      (2, 'CATEGORY', 1, 1, 2, 'FUNCTION', 1, 'LOCALITY-2', 1),
       (3, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-3', 2),
       (4, 'CATEGORY', 1, 1, 1, 'FUNCTION', 2, 'LOCALITY-4', 2),
       (5, 'CATEGORY', 1, 1, 1, 'FUNCTION', 3, 'LOCALITY-5', 3);
