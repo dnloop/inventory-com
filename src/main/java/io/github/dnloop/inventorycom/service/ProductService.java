5b3eed6357fe3a6b6ea9ad482a63fcd6e216b1ed
dnloop <dnloopdev@gmail.com>
2021-04-01 18:29:06 -0300

    Added basic repositories implementation.

--- src/main/java/io/github/dnloop/inventorycom/service/ProductService.java
+++ src/main/java/io/github/dnloop/inventorycom/service/ProductService.java
@@ -1,12 +1,20 @@
 package io.github.dnloop.inventorycom.service;
 
+import io.github.dnloop.inventorycom.model.*;
 import io.github.dnloop.inventorycom.repository.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductService {
@@ -15,7 +23,11 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    private final ProductDetailRepository productDetailRepository;
+
     private final CategoryRepository categoryRepository;
+
+    private final CategoryLevelRepository categoryLevelRepository;
 
     private final MeasureRepository measureRepository;
 
@@ -36,15 +48,257 @@ public class ProductService {
 
     public ProductService(
             ProductRepository productRepository,
+            ProductDetailRepository productDetailRepository,
             CategoryRepository categoryRepository,
+            CategoryLevelRepository categoryLevelRepository,
             MeasureRepository measureRepository,
             PresentationRepository presentationRepository,
             MaterialRepository materialRepository
     ) {
         this.productRepository = productRepository;
+        this.productDetailRepository = productDetailRepository;
         this.categoryRepository = categoryRepository;
+        this.categoryLevelRepository = categoryLevelRepository;
         this.measureRepository = measureRepository;
         this.presentationRepository = presentationRepository;
         this.materialRepository = materialRepository;
+    }
+
+    /* Product */
+
+    @Async
+    public CompletableFuture<Optional<Product>> findProductById(int id) {
+        return CompletableFuture.completedFuture(productRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<Product>> findProductDeleted(Integer id) {
+        return CompletableFuture.completedFuture(productRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<Page<Product>> findAllProducts() {
+        return CompletableFuture.completedFuture(productRepository.findAll(pageableFifty));
+    }
+
+    @Async
+    public CompletableFuture<Page<Product>> findAllProducts(Pageable pageable) {
+        return CompletableFuture.completedFuture(productRepository.findAll(pageable));
+    }
+
+    @Async
+    public CompletableFuture<Page<Product>> findProductAllDeleted() {
+        return CompletableFuture.completedFuture(productRepository.findAllDeleted(pageableFiftyDeleted));
+    }
+
+    @Async
+    public CompletableFuture<Page<Product>> findProductAllDeleted(Pageable pageable) {
+        return CompletableFuture.completedFuture(productRepository.findAllDeleted(pageable));
+    }
+
+    @Async
+    public CompletableFuture<Product> saveProduct(Product product) {
+        return CompletableFuture.completedFuture(productRepository.save(product));
+    }
+
+
+    @Transactional
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+        log.debug("Record Deleted: " + product.toString());
+        log.debug("Deleting Relationships");
+        ProductDetail productDetail = product.getProductDetailByDetailId();
+        log.debug("Product Detail Deleted.");
+        productDetailRepository.delete(productDetail);
+    }
+
+    /* Category */
+
+    @Async
+    public CompletableFuture<Optional<Category>> findCategoryById(int id) {
+        return CompletableFuture.completedFuture(categoryRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<Category>> findDeletedCategory(Integer id) {
+        return CompletableFuture.completedFuture(categoryRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<HashSet<Category>> findAllCategory() {
+        return CompletableFuture.completedFuture(categoryRepository.findAll());
+    }
+
+    @Async
+    public CompletableFuture<HashSet<Category>> findAllDeletedCategory() {
+        return CompletableFuture.completedFuture(categoryRepository.findAllDeleted());
+    }
+
+    @Async
+    public CompletableFuture<Category> saveCategory(Category category) {
+        return CompletableFuture.completedFuture(categoryRepository.save(category));
+    }
+
+    @Transactional
+    public void deleteCategory(Category category) {
+        categoryRepository.delete(category);
+        log.debug("Record Deleted: " + category.toString());
+        log.debug("Deleting Relationships");
+        log.debug("Record Deleted: ");
+    }
+
+    /* Category Level */
+
+    @Async
+    public CompletableFuture<Optional<CategoryLevel>> findCategoryLevelById(int id) {
+        return CompletableFuture.completedFuture(categoryLevelRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<CategoryLevel>> findDeletedCategoryLevel(Integer id) {
+        return CompletableFuture.completedFuture(categoryLevelRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<HashSet<CategoryLevel>> findAllCategoryLevel() {
+        return CompletableFuture.completedFuture(categoryLevelRepository.findAll());
+    }
+
+    @Async
+    public CompletableFuture<HashSet<CategoryLevel>> findAllDeletedCategoryLevel() {
+        return CompletableFuture.completedFuture(categoryLevelRepository.findAllDeleted());
+    }
+
+    @Async
+    public CompletableFuture<CategoryLevel> saveCategoryLevel(CategoryLevel category) {
+        return CompletableFuture.completedFuture(categoryLevelRepository.save(category));
+    }
+
+    @Transactional
+    public void deleteCategoryLevel(CategoryLevel category) {
+        categoryLevelRepository.delete(category);
+        log.debug("Record Deleted: " + category.toString());
+        log.debug("Deleting Relationships");
+        log.debug("Record Deleted: ");
+    }
+
+    /* Measure */
+
+    @Async
+    public CompletableFuture<Optional<Measure>> findMeasrueById(int id) {
+        return CompletableFuture.completedFuture(measureRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<Measure>> findDeletedMeasure(Integer id) {
+        return CompletableFuture.completedFuture(measureRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<Page<Measure>> findAllMeasures() {
+        return CompletableFuture.completedFuture(measureRepository.findAll(pageableFifty));
+    }
+
+    @Async
+    public CompletableFuture<Page<Measure>> findAllMeasures(Pageable pageable) {
+        return CompletableFuture.completedFuture(measureRepository.findAll(pageable));
+    }
+
+    @Async
+    public CompletableFuture<Page<Measure>> findAllDeletedMeasures() {
+        return CompletableFuture.completedFuture(measureRepository.findAllDeleted(pageableFiftyDeleted));
+    }
+
+    @Async
+    public CompletableFuture<Measure> saveMeasures(Measure measure) {
+        return CompletableFuture.completedFuture(measureRepository.save(measure));
+    }
+
+    @Transactional
+    public void deleteMeasures(Measure measure) {
+        measureRepository.delete(measure);
+        log.debug("Record Deleted: " + measure.toString());
+        log.debug("Deleting Relationships");
+        log.debug("Record Deleted: ");
+    }
+
+    /* Presentation */
+
+    @Async
+    public CompletableFuture<Optional<Presentation>> findPresentationById(int id) {
+        return CompletableFuture.completedFuture(presentationRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<Presentation>> findDeletedPresentation(Integer id) {
+        return CompletableFuture.completedFuture(presentationRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<Page<Presentation>> findAllPresentations() {
+        return CompletableFuture.completedFuture(presentationRepository.findAll(pageableFifty));
+    }
+
+    @Async
+    public CompletableFuture<Page<Presentation>> findAllPresentations(Pageable pageable) {
+        return CompletableFuture.completedFuture(presentationRepository.findAll(pageable));
+    }
+
+    @Async
+    public CompletableFuture<Page<Presentation>> findAllDeletedPresentations() {
+        return CompletableFuture.completedFuture(presentationRepository.findAllDeleted(pageableFiftyDeleted));
+    }
+
+    @Async
+    public CompletableFuture<Presentation> savePresentation(Presentation presentation) {
+        return CompletableFuture.completedFuture(presentationRepository.save(presentation));
+    }
+
+    @Transactional
+    public void deletePresentation(Presentation presentation) {
+        presentationRepository.delete(presentation);
+        log.debug("Record Deleted: " + presentation.toString());
+        log.debug("Deleting Relationships");
+        log.debug("Record Deleted: ");
+    }
+
+    /* Material */
+
+    @Async
+    public CompletableFuture<Optional<Material>> findById(int id) {
+        return CompletableFuture.completedFuture(materialRepository.findById(id));
+    }
+
+    @Async
+    public CompletableFuture<Optional<Material>> findDeleted(Integer id) {
+        return CompletableFuture.completedFuture(materialRepository.findDeleted(id));
+    }
+
+    @Async
+    public CompletableFuture<Page<Material>> findAll() {
+        return CompletableFuture.completedFuture(materialRepository.findAll(pageableFifty));
+    }
+
+    @Async
+    public CompletableFuture<Page<Material>> findAll(Pageable pageable) {
+        return CompletableFuture.completedFuture(materialRepository.findAll(pageable));
+    }
+
+    @Async
+    public CompletableFuture<Page<Material>> findAllDeleted() {
+        return CompletableFuture.completedFuture(materialRepository.findAllDeleted(pageableFiftyDeleted));
+    }
+
+    @Async
+    public CompletableFuture<Material> save(Material material) {
+        return CompletableFuture.completedFuture(materialRepository.save(material));
+    }
+
+    @Transactional
+    public void delete(Material material) {
+        materialRepository.delete(material);
+        log.debug("Record Deleted: " + material.toString());
+        log.debug("Deleting Relationships");
+        log.debug("Record Deleted: ");
     }
 }
