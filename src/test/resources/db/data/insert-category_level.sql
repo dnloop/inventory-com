8c3f6a27260d66153a9f00df795a086efb1974df
dnloop <dnloopdev@gmail.com>
2021-04-09 08:22:40 -0300

    Updated test data.

--- src/test/resources/db/data/insert-category_level.sql
+++ src/test/resources/db/data/insert-category_level.sql
@@ -1,9 +1,9 @@
 INSERT INTO PUBLIC.CATEGORY_LEVEL
 (CATEGORY_ID, L1, L2, L3, L4, CREATED_AT, DELETED, DELETED_AT, MODIFIED_AT)
 VALUES
-(1, 1, 1, 0, 0, CURRENT_TIMESTAMP(), 0, null, null),
-(2, 2, 1, 1, 0, CURRENT_TIMESTAMP(), 0, null, null),
-(3, 3, 1, 2, 0, CURRENT_TIMESTAMP(), 0, null, null),
-(4, 4, 1, 1, 1, CURRENT_TIMESTAMP(), 0, null, null),
-(5, 5, 2, 0, 0, CURRENT_TIMESTAMP(), 0, null, null),
-(6, 6, 0, 0, 0, CURRENT_TIMESTAMP(), 0, null, null);
+(1, 1, 0, 0, 0, CURRENT_TIMESTAMP(), 0, null, null),
+(2, 1, 1, 0, 0, CURRENT_TIMESTAMP(), 0, null, null),
+(3, 1, 2, 1, 0, CURRENT_TIMESTAMP(), 0, null, null),
+(4, 1, 2, 2, 0, CURRENT_TIMESTAMP(), 0, null, null),
+(5, 2, 0, 0, 0, CURRENT_TIMESTAMP(), 0, null, null),
+(6, 2, 1, 0, 0, CURRENT_TIMESTAMP(), 0, null, null);
