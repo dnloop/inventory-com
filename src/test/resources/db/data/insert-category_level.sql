a5ddf284cd572afbb7eddba8411d3546285cca91
dnloop <dnloopdev@gmail.com>
2021-04-10 17:56:30 -0300

    Added license header.

--- src/test/resources/db/data/insert-category_level.sql
+++ src/test/resources/db/data/insert-category_level.sql
@@ -1,3 +1,22 @@
+/*
+ *     Inventory-Com: Inventory and Commerce Management Application.
+ *     Copyright (C) 2021. dnloop.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 INSERT INTO PUBLIC.CATEGORY_LEVEL
 (CATEGORY_ID, L1, L2, L3, L4, CREATED_AT, DELETED, DELETED_AT, MODIFIED_AT)
 VALUES
