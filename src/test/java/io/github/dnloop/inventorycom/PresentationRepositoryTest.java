e73488203bb10ec82d2b8467ea13d9087fa5c191
dnloop <dnloopdev@gmail.com>
2021-04-15 10:26:26 -0300

    Added license header, adjusted constructor.

--- src/test/java/io/github/dnloop/inventorycom/PresentationRepositoryTest.java
+++ src/test/java/io/github/dnloop/inventorycom/PresentationRepositoryTest.java
@@ -1,3 +1,22 @@
+/*
+ *     Inventory-Com: Inventory and Commerce Management Application.
+ *     Copyright (C) 2021. dnloop.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package io.github.dnloop.inventorycom;
 
 import io.github.dnloop.inventorycom.model.Presentation;
@@ -115,8 +134,11 @@ public class PresentationRepositoryTest {
     @Test
     void savePresentation() throws ExecutionException, InterruptedException {
         Product newProduct = new Product(
-                "PD-NEW", 1,
-                new BigDecimal("200.00")
+                "PD-NEW",
+                1,
+                new BigDecimal("200.00"),
+                2,
+                1
         );
 
         final CompletableFuture<Optional<Product>> product =
