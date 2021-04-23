b29039c189e43bceda76721681289478bf01d11b
dnloop <dnloopdev@gmail.com>
2021-04-23 10:16:28 -0300

    Adjusted query methods to reflect current repository.

--- src/main/java/io/github/dnloop/inventorycom/repository/MaterialRepository.java
+++ src/main/java/io/github/dnloop/inventorycom/repository/MaterialRepository.java
@@ -4,6 +4,7 @@ import io.github.dnloop.inventorycom.model.Material;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -28,4 +29,25 @@ public interface MaterialRepository extends JpaRepository<Material, Integer> {
            " WHERE material.id = :id" +
            " AND material.deleted = 1")
     Optional<Material> findDeleted(int id);
+
+    /**
+     * Returns the number of assigned materials to a Product Detail.
+     */
+    @Query("SELECT COUNT (productDetail.id)" +
+           " FROM ProductDetail productDetail" +
+           " WHERE productDetail.materialId = :materialId" +
+           " AND productDetail.deleted = 0")
+    Integer existsInProductDetail(int materialId);
+
+    /**
+     * Method to delete a single material.
+     * <p>
+     * By using this method we ensure Product Detail is not in an invalid state. If
+     * a record is 'deleted' we assign a default 'unavailable' value.
+     */
+    @Modifying
+    @Query("UPDATE Material material" +
+           " SET material.deleted = 1" +
+           " WHERE material.id = :materialId")
+    void delete(int materialId);
 }
